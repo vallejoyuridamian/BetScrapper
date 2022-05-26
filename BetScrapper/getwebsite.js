9cecfe9ff9f8751d9a1cc8635918eb7fa2f4ea78
vallejoyuridamian <vallejoyuridamian@gmail.com>
2022-05-26 15:12:21 -0300

    upload scrapper

---
+++ BetScrapper/getwebsite.js
@@ -0,0 +1,16 @@
+
+	var webPage = require('webpage');
+	var page = webPage.create();
+	
+	page.settings.loadImages  = false;
+	page.settings.resourceTimeout = 6000;
+
+	page.open('https://www.supermatch.com.uy/live#/31604317', function(status) {
+	  
+	  setTimeout(function() {
+		console.log(page.content);
+		phantom.exit();
+	  }, 6000);
+    });
+	
+	
